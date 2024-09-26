ec354696e9d2dbe5d632c061207950c06c9df93a
chiagoziepio <nduagubapio@gmail.com>
2024-09-26 13:30:57 +0100

    about page done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/Pages/Home";
 import AllDoctors from "./components/Pages/AllDoctors";
+import About from "./components/Pages/About";
+import Contact from "./components/Pages/Contact";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/doctors" element={<AllDoctors />} />
+          <Route path = "/about" element={<About/>}/>
+          <Route path = "/contact" element={<Contact/>}/>
         </Route>
       </Routes>
     </main>
---
+++ src/components/Pages/About.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="h-full flex-grow res p-[10px]">
+      <div>
+        <h3 className="outfit-medium text-[24px] md:text-[30px] text-center">
+          About CarePoint
+        </h3>
+        <div className="flex flex-col gap-y-[20px] md:flex-row gap-x-[3px] w-full">
+          <div className="md:w-[30%] md:h-[300px]">
+            <img src="/assests/about_image.svg" alt="" className="w-full h-full" />
+          </div>
+          <div className="outfit-medium text-[18px] md:w-[60%]">
+            <p>
+              Welcome to CarePoint, your trusted partner in managing your
+              healthcare needs conveniently and efficiently. At Prescripto, we
+              understand the challenges individuals face when it comes to
+              scheduling doctor appointments and managing their health records.{" "}
+              <br /> <br />
+              CarePoint is committed to excellence in healthcare technology. We
+              continuously strive to enhance our platform, integrating the
+              latest advancements to improve user experience and deliver
+              superior service. Whether you're booking your first appointment or
+              managing ongoing care, Prescripto is here to support you every
+              step of the way.
+            </p>{" "}
+            <br />
+            <div>
+              <span className="outfit-bold">Our Vision</span>
+              <p>
+                Our vision at Prescripto is to create a seamless healthcare
+                experience for every user. We aim to bridge the gap between
+                patients and healthcare providers, making it easier for you to
+                access the care you need, when you need it.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
